51ee9ef894e9d640181f6cc4f4f194c9797dcf9a
xsadia <lipearthur@hotmail.com>
2020-12-23 14:31:53 -0300

    first commit :monkey:

---
+++ src/styles/global.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: #d6d6d6;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
